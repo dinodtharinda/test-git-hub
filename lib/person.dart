6c4a3d65c3ae4d7bf1a4a01f0ce5744a04fe88f0
dinod <dinodtharinda2001@gmail.com>
2026-01-09 20:14:43 +0530

    Add gender property to Person class constructor

--- lib/person.dart
+++ lib/person.dart
@@ -1,6 +1,7 @@
 class Person{
   String name;
   int age;
+  String gender;
 
-  Person({required this.name, required this.age});
+  Person({required this.name, required this.age, required this.gender});
 }
